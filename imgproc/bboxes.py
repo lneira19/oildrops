1226fa7273a348dcaea4724ca1c577eabbe5f158
lneira19 <lneira@itba.edu.ar>
2025-06-03 12:32:45 -0300

    Added bounding box filtering by minarea.

--- imgproc/bboxes.py
+++ imgproc/bboxes.py
@@ -13,7 +13,7 @@ class Bbox:
         self.hsv_lower_limit =  hsv_lower_limit
         self.hsv_upper_limit = hsv_upper_limit
     
-    def getBoundingBoxesForImg(self,bgr_img):
+    def getBoundingBoxesForImg(self,bgr_img,sdk=-1,minarea=250):
 
         # Convertir la imagen BGR a HSV
         hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
@@ -36,7 +36,7 @@ class Bbox:
         contours, contours_img = ipbasics.getContours(mask_open)
 
         # Filtrar contornos por longitud
-        contours_filtered, contours_filtered_img = ipbasics.filterContours(mask_open, contours, mult=-0.5, mode='UP')
+        contours_filtered, contours_filtered_img = ipbasics.filterContours(mask_open, contours, mult=sdk, mode='UP')
 
         # Obtener máscaras individuales para cada contorno filtrado
         list_individual_masks = ipbasics.getIndividualMasks(mask_open,contours_filtered)
@@ -44,14 +44,17 @@ class Bbox:
         # Obtener bounding boxes para cada máscara individual
         list_bboxes = [ipbasics.getBoundingBox(mask) for mask in list_individual_masks]
 
+        # Filtrar bounding boxes que no tengan un área mínima
+        list_bboxes = [bbox for bbox in list_bboxes if (bbox[0][0] - bbox[1][0]) * (bbox[0][1] - bbox[1][1]) > minarea]
+
         return list_bboxes
     
-    def getImgWithBboxes(self, path_img):
+    def getImgWithBboxes(self, path_img, sdk=-1,minarea=250):
         
         bgr_img = cv2.imread(path_img) # Imagen BGR
         rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB) # Convertir a RGB para matplotlib
 
-        bboxes = self.getBoundingBoxesForImg(bgr_img)
+        bboxes = self.getBoundingBoxesForImg(bgr_img=bgr_img,sdk=sdk,minarea=minarea)
 
         rgb_img_copy = rgb_img.copy()
         for bbox in bboxes:
